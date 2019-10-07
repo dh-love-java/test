d5c9b6794536d255237f04d6e0b0879a3e789ae9
dh-love-java <50996892+dh-love-java@users.noreply.github.com>
2019-10-07 22:23:23 +0800

    添加方法

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,5 +1,5 @@
 public class FirstClass {
-    public static void mian(String[]args){
+    public static void main(String[] args){
         System.out.println("hello git project");
     }
     public void test(){
